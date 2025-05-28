c60cecce4068698d0369bbe9e14a9163d821b330
jeewonyang <jy4ng@caltech.edu>
2025-05-28 13:43:21 -0700

    Connect SlotScheduler to App.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,3 @@
-import logo from './logo.svg';
-import './App.css';
-import SlotScheduler from './SlotScheduler';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 import SlotScheduler from './SlotScheduler';
 
 function App() {
@@ -32,5 +8,4 @@ function App() {
   );
 }
 
-export default App;
 export default App;
